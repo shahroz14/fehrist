c68cafb25370cba36e9b5a4500e103d2b4e6d8ec
shahroz14 <shahroz14@gmail.com>
2021-06-21 19:59:44 +0530

    Init repo

---
+++ src/main/java/com/nikaah/fehrist/FehristApplication.java
@@ -0,0 +1,13 @@
+package com.nikaah.fehrist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FehristApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FehristApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nikaah/fehrist/FehristApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikaah.fehrist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FehristApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
